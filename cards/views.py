33183ec2cb60bef048c3114dc197783ef28ac7a8
Alexander Payne <81712870+DrPayne25@users.noreply.github.com>
2022-03-26 16:17:27 -0700

    Revert "Jwt"

--- cards/views.py
+++ cards/views.py
@@ -3,14 +3,13 @@ from rest_framework import generics
 from .models import Card
 from .serializer import CardSerializer
 from .permissions import IsOwnerOrReadOnly
-from rest_framework.permissions import IsAuthenticated
 
 class CardList(generics.ListCreateAPIView):
-  permission_classes = (IsAuthenticated,)
+  permission_classes = (IsOwnerOrReadOnly,)
   queryset = Card.objects.all()
   serializer_class = CardSerializer
 
 class CardDetail(generics.RetrieveUpdateDestroyAPIView):
-  permission_classes = (IsAuthenticated,)
+  permission_classes = (IsOwnerOrReadOnly,)
   queryset = Card.objects.all()
   serializer_class = CardSerializer
